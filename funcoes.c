8b8863807d5bc7e04ee493b2458033dc9d96f60a
americopenga20-jpg <americopenga20@gmail.com>
2026-03-27 14:14:44 +0200

    Refactor sorting functions in funcoes.c
    Removed selectionSort and insertionSort functions, added bubbleSort call in main.

--- funcoes.c
+++ funcoes.c
@@ -1,40 +1,16 @@
-
+#include <stdio.h>
 #include "funcoes.h"
 
-void bubbleSort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-}
+int main() {
+    int vetor[] = {8,3,5,2,9,1,4};
+    int n = 7;
 
-void selectionSort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
-        int min = i;
-        for(int j=i+1;j<n;j++){
-            if(arr[j] < arr[min]){
-                min = j;
-            }
-        }
-        int temp = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
-    }
-}
+    bubbleSort(vetor, n);
 
-void insertionSort(int arr[], int n){
-    for(int i=1;i<n;i++){
-        int key = arr[i];
-        int j = i-1;
-        while(j>=0 && arr[j] > key){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = key;
+    printf("Vetor ordenado:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
     }
+
+    return 0;
 }

7e0347dfc3dcac0131555c273ef3f14da724a605
americopenga20-jpg <americopenga20@gmail.com>
2026-03-27 14:18:23 +0200

    Refactor main function and streamline output
    Refactor main function for improved readability and efficiency.

--- main.c
+++ main.c
@@ -1,25 +1,15 @@
-
 #include <stdio.h>
 #include "funcoes.h"
 
-int main(){
-
+int main() {
     int vetor[] = {8,3,5,2,9,1,4};
     int n = 7;
 
-    printf("Vetor original:\n");
-    for(int i=0;i<n;i++){
-        printf("%d ",vetor[i]);
-    }
-
-    printf("\n\nOrdenando com Bubble Sort...\n");
-
-    bubbleSort(vetor,n);
+    bubbleSort(vetor, n);
 
     printf("Vetor ordenado:\n");
-
-    for(int i=0;i<n;i++){
-        printf("%d ",vetor[i]);
+    for(int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
     }
 
     return 0;

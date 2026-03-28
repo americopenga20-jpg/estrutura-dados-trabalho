d846e64827bf3343d608ce62e693d1212663b5ff
americopenga20-jpg <americopenga20@gmail.com>
2026-03-28 11:52:40 +0200

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -2,3 +2,38 @@
 void bubbleSort(int arr[], int n);
 void selectionSort(int arr[], int n);
 void insertionSort(int arr[], int n);
+[11:49, 28/03/2026] J@€L Unilicungo: void bubbleSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+[11:49, 28/03/2026] J@€L Unilicungo: void selectionSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        int min = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+[11:49, 28/03/2026] J@€L Unilicungo: void insertionSort(int arr[], int n){
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
